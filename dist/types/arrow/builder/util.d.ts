42c97213a78ab491084159537002deb50481c9f7
serhat.kanbas <serhat.kanbas@prireis.com.tr>
2022-03-15 16:37:04 +0300

    fix: Added dist folder for testing

---
+++ dist/types/arrow/builder/util.d.ts
@@ -0,0 +1,8 @@
+export function ceil64Bytes(length: any, bpe?: number): number;
+export function array(Type: any, length: any, bpe?: any): any;
+export function arrowData(d: any): any;
+export function arrowVector(data: any): any;
+export function encode(data: any, idx: any, str: any): number;
+export function encodeInto(data: any, idx: any, str: any): number;
+export const encoder: TextEncoder;
+export function writeUtf8(data: any, idx: any, str: any): number;
---
+++ dist/types/arrow/encode/data-from.d.ts
@@ -0,0 +1,2 @@
+export function dataFromArray(array: any, type: any): any;
+export function dataFromScan(nrows: any, scan: any, column: any, type: any, nullable?: boolean): any;
